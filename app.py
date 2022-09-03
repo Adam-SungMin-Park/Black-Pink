ac4e7cd59c40b3537c0b406c58661e4fdea471f8
Adam Park <parksm2@uci.edu>
2022-09-03 17:01:23 +0900

    uploading picture

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
@@ -6,13 +7,35 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.o2cbi29.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/test')
 def test():
     return render_template('test.html')
+
+
+@app.route("/test", methods=["POST"])
+def picture_post():
+    image_receive = request.form['image_give']
+    name_receive = request.form['name_give']
+
+    doc = {
+        'name': name_receive,
+        'image': image_receive
+    }
+    db.fansGallery.insert_one(doc)
+    return jsonify({'msg': 'picture uploaded!'})
+
+
+@app.route('/test', methods=['GET'])
+def picture_get():
+    all_picture = list(db.fansGallery.find({},{'_id': False}))
+    return jsonify({'msg': all_picture })
+
 
 
 @app.route("/homework", methods=["POST"])
@@ -21,16 +44,18 @@ def homework_post():
     comment_receive = request.form['comment_give']
 
     doc = {
-        'name':name_receive,
-        'comment':comment_receive
+        'name': name_receive,
+        'comment': comment_receive
     }
-    db.fans.insert_one(doc);
-    return jsonify({'msg':'POSTED!'})
+    db.fans.insert_one(doc)
+    return jsonify({'msg': 'POSTED!'})
+
 
 @app.route("/homework", methods=["GET"])
 def homework_get():
-    allFans = list(db.fans.find({},{'_id': False}))
+    allFans = list(db.fans.find({}, {'_id': False}))
     return jsonify({'msg': allFans})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
